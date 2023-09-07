b790c51e63dce88869fb650861bbb58a3c530b91
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-09-08 01:25:13 +0600

    image

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,18 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string().max(90),
-    description: z.string().max(160),
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    tags: z.array(z.string()),
-    draft: z.boolean().default(false),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().max(90),
+      description: z.string().max(160),
+      image: image().optional(),
+      pubDate: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      tags: z.array(z.string()),
+      draft: z.boolean().default(false),
+    }),
 });
 
 export const collections = { blog };
